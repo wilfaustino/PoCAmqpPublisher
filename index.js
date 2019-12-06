23249adf94b16fd7e2df0c922405075fe11ffd55
joseteodoro <jteodoro.dev@gmail.com>
2019-12-06 18:11:15 -0300

    adding rabbit publisher

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
 const app = express()
+const amqp = require('amqplib')
 
-// parse application/json
+// const RABBIT_DEFAULT_URL = 'amqp://guess:guess@127.0.0.1'
+const RABBIT_DEFAULT_URL = 'amqp://127.0.0.1'
+const DEFAULT_PORT = 4000
+
 app.use(bodyParser.json())
 
-app.post('/message', function (req, res) {
-    console.log('body: ' , req.body)
-    res.send('Hello World!')
+app.get('/health', function (_, res) {
+    res.send('ok')
 })
 
-const DEFAULT_PORT = 4000
+app.post('/:queue/', function (req, res) {
+    const { body } = req
+    const { queue } = req.params
+    console.log('called' , { queue, body })
+
+    const open = amqp.connect(process.env.RABBIT_URL || RABBIT_DEFAULT_URL)
+    open.then(conn => {
+        return conn.createChannel()
+    })
+    .then(ch => {
+        return ch.assertQueue(queue)
+            .then(() => {
+                return ch.sendToQueue(queue, Buffer.from(JSON.stringify(body)))
+            })
+    })
+    .then(() => {
+        console.log(`Published to ${queue}`)
+        res.status(200).send(`Published to ${queue}.`)
+    })
+    .catch(err => {
+        res.status(500).send(`Error pushing to ${queue}. ${err}`)
+    })
+})
 
 console.log(`Starting server on port ${process.env.PORT || DEFAULT_PORT}`)
 app.listen(process.env.PORT || DEFAULT_PORT)
